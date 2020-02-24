3c8eed31abf75dd62c263d2c98237b84215ef944
Larry Tseng <larry.tseng.1@gmail.com>
2020-02-24 14:02:58 -0500

    working version 1

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
+// Readit
+// Larry Tseng
+
 const redditBase = "https://reddit.com/";
 const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+
+let currentSubreddit = "";
+
+/**
+ * REFERENCE:
+ * id: post.name,
+ * url: post.permalink,
+ * title: post.title,
+ * author: post.author,
+ * body: text,
+ * showImage: showImage,
+ * thumbnail: image,
+ * alt: post.title
+ */
 
 /**
  * Concatenates a Reddit sub-path with a CORS proxy and appends .json.
  *
  * @param item - the path excluding the domain
+ * @param after
  * @returns string
  */
-function urlToJSON(item) {
-    return corsAnywhere + redditBase + item + ".json";
+function urlToJSON(item, after) {
+    return corsAnywhere + redditBase + item + ".json" + after;
 }
 
 /**
  * Calls the Reddit API via JQuery/AJAX.
  *
  * @param subreddit - a string of the subreddit to call
+ * @param after
  * @param successFn - a callback upon success
  * @param failureFn - a callback upon failure
  * @param alwaysFn - a callback that cleans up any left over process, always called
  */
-function getSubreddit(subreddit, successFn, failureFn, alwaysFn) {
-    $.ajax(urlToJSON("r/" + subreddit), {
+function getSubreddit(subreddit, after, successFn, failureFn, alwaysFn) {
+    $.ajax(urlToJSON("r/" + subreddit, after), {
         type: 'GET',
         crossDomain: true,
         success: successFn,
@@ -30,6 +49,9 @@ function getSubreddit(subreddit, successFn, failureFn, alwaysFn) {
 }
 
 function checkImageURL(url) {
+    if (url === undefined) {
+        return false;
+    }
     return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
 
@@ -57,6 +79,7 @@ function cleanUpJSON(posts) {
             text = text.slice(0, 250) + "...";
         }
         postArray.push({
+            id: post.name,
             url: post.permalink,
             title: post.title,
             author: post.author,
@@ -79,17 +102,23 @@ function cleanUpJSON(posts) {
  * @param template - the HTML template to be rendered (string matching ID)
  * @param content - an object of objects with post information
  * @param location
+ * @param newFeed
  */
-function renderContent(template, content, location) {
+function renderContent(template, content, location, newFeed) {
     let html = document.getElementById(template).innerHTML;
-    document.getElementById(location).innerHTML = Mustache.render(html, content);
+    if (newFeed) {
+        document.getElementById(location).innerHTML = "";
+    }
+    document.getElementById(location).innerHTML += Mustache.render(html, content);
 }
 
 // Document is ready
 $(document).ready(() => {
     console.log("Document is ready.");
+
+    // Enter "popular" into the feed and get results (so it's not a blank page)
     $("#subreddit-field").val("popular");
-    // $("#subreddit-submit").click();
+    $("#subreddit-submit").click();
 });
 
 
@@ -112,20 +141,19 @@ $("#subreddit-submit").click(() => {
         element.addEventListener('animationend', () => element.classList.remove("animated", "shake"));
     };
 
-    let subreddit = $("#subreddit-field").val();
-    if (subreddit === "") {
+    currentSubreddit = $("#subreddit-field").val();
+    if (currentSubreddit === "") {
         failFn();
         afterFn();
         return;
     }
 
-    console.log("Getting subreddit: " + subreddit);
+    console.log("Getting subreddit: " + currentSubreddit);
 
     // animate submit button to loading
     $("#subreddit-submit").html("<i class='fas fa-cog fa-spin'></i>");
 
-    let result;
-    getSubreddit(subreddit, (data) => {
+    getSubreddit(currentSubreddit, "", (data) => {
         console.log("Successfully completed the AJAX request.");
 
         // reset color
@@ -133,12 +161,13 @@ $("#subreddit-submit").click(() => {
         $("#subreddit-submit").removeClass("btn-outline-danger");
 
         if (data.data.children.length > 0) {
-            renderContent("post-template", cleanUpJSON(data), "posts");
+            renderContent("post-template", cleanUpJSON(data), "posts", true);
         } else {
             failFn();
             afterFn();
         }
 
+        $("#load-more").prop("disabled", false);
 
     }, () => {
         console.error("Unable to complete the request.");
@@ -148,3 +177,28 @@ $("#subreddit-submit").click(() => {
         afterFn();
     });
 });
+
+// Fetch more posts from Reddit if requested
+$("#load-more").click(() => {
+    if (currentSubreddit !== "" && $("#posts").children().length > 0) {
+        // Get ID of last post
+        let after = $(".post").last().prop("id");
+
+        // prevents elements that are loaded from re-animating upon new data
+        $(".card").removeClass("animated");
+
+        // Call getSubreddit(...) and render posts
+        getSubreddit(currentSubreddit, "?after=" + after, (data) => {
+            console.log("Loading more posts was successful");
+            if (data.data.children.length > 0) {
+                renderContent("post-template", cleanUpJSON(data), "posts", false);
+            }
+        }, () => {
+            console.error("Could not load more posts");
+            $("#load-more").prop("disabled", true);
+        })
+    }
+});
+
+// TESTS
+console.assert(urlToJSON("r/popular", "") === "https://cors-anywhere.herokuapp.com/https://reddit.com/r/popular.json", "URL matching");
